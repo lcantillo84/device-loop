c3faef95bda07cef9665ab92b9778230707c114f
lcantillo84 <lcantillodev@gmail.com>
2025-07-04 09:45:25 -0400

    x

--- backend/Program.cs
+++ backend/Program.cs
@@ -43,7 +43,7 @@ using (var scope = app.Services.CreateScope())
     var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
     var seeder = scope.ServiceProvider.GetRequiredService<DeviceDataSeeder>();
     
-    await context.Database.EnsureCreatedAsync();
+    context.Database.Migrate();
     await seeder.SeedFromJsonAsync("Data/test.json");
 }
 
